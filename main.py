0099618698ff53dca14ad8b37d8eb14f03671ff2
uneconomicalfairy14 <tanmay404103@outlook.com>
2022-01-09 16:30:36 +0530

    bug fix

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ while cam.isOpened():
     blur = cv2.GaussianBlur(gray, (5, 5), 0)
     _, thresh = cv2.threshold(blur, 20, 255, cv2.THRESH_BINARY)
     dilated = cv2.dilate(thresh, None, iterations=3)
-    contours, _ = cv2.findContours(dilated, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    _, contours, _ = cv2.findContours(dilated, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     cv2.drawContours(frame1, contours, -1, (0, 255, 0), 2)
     if cv2.waitKey(10) == ord('q'):
         break
